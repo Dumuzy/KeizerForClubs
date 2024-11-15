55cb51c5d31da316c27552068c39aa6ea1165e86
A <g@atlantis44.de>
2024-11-15 14:52:59 +0100

    #82: Program won't run on several of A.D.'s computers

--- create-pack.cmd
+++ create-pack.cmd
@@ -1,10 +1,11 @@
 rem
 rem     This script packs together a Keizer_...zip package. 
-rem     It uses the  executables from the bin\Release-directory. 
+rem     It uses the  executables from the bin\x86\Release-directory. 
 rem     It takes the version number from KFC2.csproj. 
-rem     To work, this script needs   tclsh.exe and 7za.exe in PATH. 
+rem     To work, this script needs robocopy,  tclsh.exe and 7za.exe in PATH. 
 
 @echo off
+set "startdir=%cd%"
 set projfile=KFC2.csproj
 rem The following seems to be the way to go to put the result of a command into 
 rem a variable in cmd-scripts. It is truly astonishing. 
@@ -27,9 +28,10 @@ del /Q %tempdir%\docs
 del /Q %tempdir%\export
 
 
-cd bin\Release\net6.0-windows
+cd bin\x86\Release\net6.0-windows
 copy KFC2.exe  %tempdir%\KeizerForClubs.exe
-copy ..\..\..\cfg\* cfg
+copy ..\..\..\..\cfg\* cfg
+robocopy ..\..\..\..\docs\ docs *.html *.png *.pdf *.txt /E
 
 
 robocopy .  %tempdir% *.dll *.json *.s3db Keizer*.html *.ini *.png *.pdf *.txt *.css *.xsl /E
@@ -46,3 +48,4 @@ del /Q %tempdir%\*.*
 rd %tempdir%
 
 echo %packname%.zip created in %temp0%
+cd "%startdir%"
